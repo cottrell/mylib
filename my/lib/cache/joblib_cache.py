1283fb7a0c497cb48c43e3167c130143fcce30b8
David Cottrell <cottrell@users.noreply.github.com>
2020-02-16 16:07:54 +0000

    update

--- my/lib/cache/joblib_cache.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-import joblib
-
-from ..paths import cache_basepath as _cache_basepath
-
-_basepath = cache_basepath / 'joblib_cache'
-
-memory = joblib.Memory(_basepath, verbose=10)
-
