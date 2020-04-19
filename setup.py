fc5426e2229545e17a8cae2a6a9b161044211425
David Cottrell <cottrell@users.noreply.github.com>
2020-04-19 17:05:54 +0100

    lib to oldlib while refactor.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(name='my-lib',
       version='0.2',
-      description='Stuff. Now in a namespace "my". Alias to mylib for backwards compat if things are broken.',
-      packages=['my.lib'],
+      description='Stuff. Now in a namespace "my". Alias to myoldlib for backwards compat if things are broken.',
+      packages=['my.oldlib'],
       zip_safe=False)
 
-setup(name='mylib',
+setup(name='myoldlib',
       version='0.2',
-      description='Stuff. Now in a namespace "my". Alias to mylib for backwards compat if things are broken.',
-      packages=['mylib'],
+      description='Stuff. Now in a namespace "my". Alias to myoldlib for backwards compat if things are broken.',
+      packages=['myoldlib'],
       zip_safe=False)
